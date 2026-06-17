cb844b4bd8b03908394183773213b58f84d37237
msmartechcompany-prog <msmartechcompany@gmail.com>
2026-06-17 02:31:21 -0700

    Simple program

---
+++ test.py
@@ -0,0 +1,12 @@
+def main(a,s,m):
+    i=int(input("ENter the  first number:"))
+    if i%2==0:
+        a=i+(+1)
+        print("The value of a is:",a)
+
+    else:
+        s=-(-i)
+        m=a*i
+        print("The value of s is:",s)
+        print("The value of m is:",m)
+main(0,0,0)
